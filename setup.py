19c6da3037f63dbbbf836077917673cfcce5a966
Fabian Lehner <fl@makonis.net>
2016-01-12 16:17:02 +0100

    version++

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
     name = 'djangocms_responsive_image',
     packages = ['djangocms_responsive_image'],
-    version = '0.0.1',
+    version = '0.0.3',
     description = 'Django CMS plugin implementing html5 responsive images',
     author = 'Fabian Lehner',
     author_email = 'fl@makonis.net',
     url = 'https://github.com/febsn/djangocms-responsive-image',
-    download_url = 'https://github.com/febsn/djangocms-responsive-image/tarball/0.0.1',
+    download_url = 'https://github.com/febsn/djangocms-responsive-image/tarball/0.0.3',
     install_requires = [
        'django-filer',
        'jsonfield',
