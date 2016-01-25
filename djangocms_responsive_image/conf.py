f4d13e38ac9fd6bf6c620201ff918af11ce93c20
Fabian Lehner <fl@makonis.net>
2016-01-25 11:47:17 +0100

    default config bugfix

--- djangocms_responsive_image/conf.py
+++ djangocms_responsive_image/conf.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 from appconf import AppConf
 
 class ResponsiveImageAppConf(AppConf):
     IMAGE_STYLE_CHOICES = {
         'default': {
+            'name': _('default'),
             'srcset': ((300, 0), (800, 0), (1600, 0), (2000, 0)),
             'sizes': None,
             'default_size': (1600, 0),
