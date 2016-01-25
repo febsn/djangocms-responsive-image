c570447bbbd78932c7a8548d3c36d03234d07596
Fabian Lehner <fl@makonis.net>
2016-01-25 11:59:17 +0100

    added MANIFEST.in, changed package name

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'djangocms_responsive_image',
+    name = 'djangocms-responsive-image',
     packages = find_packages(exclude=('tests', 'tests.*')),
     include_package_data=True,
     license='MIT License',
-    version = '0.0.8',
+    version = '0.0.9',
     description = 'Django CMS plugin implementing html5 responsive images',
     author = 'Fabian Lehner',
     author_email = 'fl@makonis.net',
     url = 'https://github.com/febsn/djangocms-responsive-image',
-    download_url = 'https://github.com/febsn/djangocms-responsive-image/tarball/0.0.8',
+    download_url = 'https://github.com/febsn/djangocms-responsive-image/tarball/0.0.9',
     install_requires = [
        'django-filer>=1.0.4',
        'jsonfield>=1.0.3',
